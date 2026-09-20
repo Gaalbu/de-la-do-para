40be312eac1c53f03ee7a40628b67ddf03f54ce5
Gaalbu <gabrielalbuquerquealencar@gmail.com>
2026-09-20 16:41:50 -0300

    chore(backend): bootstrap the Spring Boot application

---
+++ backend/src/test/java/br/com/deladopara/DeLaDoParaApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.deladopara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeLaDoParaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
