254ec1dacb4cb3fa617ebbba9da641cf80f31b3d
Gaalbu <gabrielalbuquerquealencar@gmail.com>
2026-09-20 23:43:00 -0300

    fix(e2e): use env PORT for SSR server

--- frontend/e2e/playwright.config.ts
+++ frontend/e2e/playwright.config.ts
@@ -9,9 +9,12 @@ export default defineConfig({
     trace: 'retain-on-failure',
   },
   webServer: {
-    command: 'PORT=4200 node ../dist/frontend/server/server.mjs',
+    command: 'node ../dist/frontend/server/server.mjs',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env['CI'],
     timeout: 30_000,
+    env: {
+      PORT: '4200',
+    },
   },
 });
